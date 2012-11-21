e18d8af86967b0771e0653702c9c01ab1d9eef0b
Sebastien Azimi <sebstp@gmail.com>
2012-11-21 23:26:17 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+InsomniieWeb::Application.config.secret_token = 'e187f4ab27d152be636b031c6ab00da03731a648f7702debea478e5a6d29a28d026c26b61d3ce1e1b0c36105f7c575b792b8b896fdb10f629514526c0d7b1a83'
