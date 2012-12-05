270cea225f3c185b0b6e4fb293c20e714cd3bbcd
Paul Chobert <paul@chobert.fr>
2012-12-06 00:26:24 +0100

    setup cloudfront

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -43,7 +43,7 @@ InsomniieWeb::Application.configure do
   # config.cache_store = :mem_cache_store
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server
-  config.action_controller.asset_host = "https://#{ENV['FOG_DIRECTORY']}.s3.amazonaws.com"
+  config.action_controller.asset_host = "https://d1soogrsggiygw.cloudfront.net"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )
